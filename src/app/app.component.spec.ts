3d944d884677c9c949771d13627b59c485fe571f
Yassinean <yassinedubraska@gmail.com>
2025-01-03 11:45:04 +0100

    resetuping the project and adding components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'musica' title`, () => {
+  it(`should have the 'MusicStream' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('musica');
+    expect(app.title).toEqual('MusicStream');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, musica');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MusicStream');
   });
 });
